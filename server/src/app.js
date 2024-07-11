83c1cf50fb7cd2679d0f8adefbfd93c87aea42a3
Pramod_Acharya <pramodacharya808@gmail.com>
2024-07-11 17:14:10 +0530

    modified few files

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,10 @@ import express from "express";
 import { apiVersion } from "./constants.js";
 import "dotenv/config";
 
-
 export const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "16kb" }));
 
-app.get(`${apiVersion}/api/}`, (req, res) => {
-  res.send(`All API's are running on endpoint : ${apiVersion}/ `);
+app.get(`/${apiVersion}/health`, (req, res) => {
+  res.json({ message: "API's are working ✅" }).status(200);
 });
